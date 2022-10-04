4a88f332351db322cd7066cc325743e903a7f55f
pranav madhu <pranavmadhu73@gmail.com>
2022-10-04 22:28:45 +0530

    replaced fcfs with fcfs accepting all cases

--- Os/Scheduling_algorithms/fcfs.c
+++ Os/Scheduling_algorithms/fcfs.c
@@ -1,7 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 struct process
 {
-  int id;
   int at;
   int bt;
   int ct;
@@ -10,59 +9,45 @@ struct process
 };
 int main()
 {
-  int no_process;
-  float tt_sum=0.0,wt_sum=0.0;
-  printf("\nEnter the no of processes.\n");
-  scanf("%d",&no_process);
-  struct process a[no_process];
-  printf("\nEnter the process id,arrival time and burst time of the processes.\n");
-  for(int i=0;i<no_process;i++)
+  int process_no;
+  float avg_tt = 0.0, avg_wt = 0.0;
+  printf("Enter the number of process \n");
+  scanf("%d", &process_no);
+  printf("Enter the arrival time and burst time of each processes\n");
+  struct process a[process_no];
+  for (int i = 0; i < process_no; i++)
   {
-    scanf("%d %d %d",&a[i].id,&a[i].at,&a[i].bt);
+    scanf("%d %d", &a[i].at, &a[i].bt);
   }
-  for(int i =0;i<no_process;i++)
+  for (int i = 0; i < process_no; i++)
   {
-    int min =i;
-    for(int j =i;j<no_process;j++)
+    for (int j = 0; j < process_no - i - 1; j++)
     {
-      if(a[j].at<a[min].at)
+      if (a[j].at > a[j + 1].at)
       {
-        min =j;
+        struct process temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
       }
     }
-    if(min !=i)
+  }
+  a[0].ct = a[0].at + a[0].bt;
+  a[0].tt = a[0].ct - a[0].at;
+  a[0].wt = a[0].tt - a[0].bt;
+  for (int i = 1; i < process_no; i++)
+  {
+    if (a[i].at <= a[i - 1].ct)
     {
-      int temp = a[min].at;
-      a[min].at = a[i].at;
-      a[i].at = temp;
-
-      temp = a[min].id;
-      a[min].id = a[i].id;
-      a[i].id = temp;
-
-      temp = a[min].bt;
-      a[min].bt = a[i].bt;
-      a[i].bt = temp;
+      a[i].ct = a[i - 1].ct + a[i].bt;
     }
+    else
+    {
+      a[i].ct = a[i].at + a[i].bt;
+    }
+    a[i].tt = a[i].ct - a[i].at;
+    a[i].wt = a[i].tt - a[i].bt;
+    avg_tt = avg_tt + a[i].tt;
+    avg_wt = avg_wt + a[i].wt;
   }
-  a[0].ct =a[0].bt;
-  for(int i =1;i<no_process;i++)
-  {
-    a[i].ct = a[i-1].ct +a[i].bt;
-  }
-  for(int i =0;i<no_process;i++)
-  {
-    a[i].tt = a[i].ct -a[i].at;
-    a[i].wt = a[i].tt -a[i].bt;
-    tt_sum +=a[i].tt;
-    wt_sum+=a[i].wt;
-  }
-  printf("\nID\t\tAT\t\tBT\t\tCT\t\tTT\t\tWT\n");
-  for(int i=0;i<no_process;i++)
-  {
-    printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",a[i].id,a[i].at,a[i].bt,a[i].ct,a[i].tt,a[i].wt);
-    
-  }
-  printf("The average Turn around time is :%f.\nThe average Waiting time is :%f",(tt_sum/no_process),(wt_sum/no_process));
-  return 0;
+  printf("The average turn around time and waiting tie is %f and %f\n", ((avg_tt + a[0].tt) / process_no), ((avg_wt + a[0].wt) / process_no));
 }
