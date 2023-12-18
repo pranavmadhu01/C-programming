3fe583ec2d7542ce79de83be662649debe8bd49e
Pranav Madhu <pranavmadhu73@gmail.com>
2023-12-18 17:31:54 +0530

    COmpiler Design - first and follow updated

--- CompilerDesign/firstfollow/ff.c
+++ CompilerDesign/firstfollow/ff.c
@@ -1,108 +1,95 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int n;
-char p[20][20], f[20];
-int k = 0;
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+char p[20][20];
+char f[20];
+int m,n;
 void first(char c);
 void follow(char c);
 int main()
 {
-    int ch=1;
-    char c;
-    printf("Enter the number of productions.\n");
-    scanf("%d", &n);
-    printf("Enter the productions.\n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%s", p[i]);
-    }
-    do
-    {
-        printf("Enter the symbol to find the first and follow.\n");
-        scanf(" %c", c);
-        first(c);
-        printf("FIRST(%c) = {", c);
-        for (int i = 0; i < k; i++)
-        {
-            if (i == k - 1)
-            {
-                printf("%c}\n", f[i]);
-            }
-            else
-            {
-                printf("%c,", f[i]);
-            }
-        }
-        k = 0;
-        strcpy(f, " ");
-        printf("FOLLOW(%c) = {", c);
-        for (int i = 0; i < k; i++)
-        {
-            if (i == k - 1)
-            {
-                printf("%c}\n", f[i]);
-            }
-            else
-            {
-                printf("%c,", f[i]);
-            }
-        }
-        printf("Do you want to continue?(0/1)");
-        scanf("%d", &ch);
-    } while (ch == 1);
-    return 0;
+	printf("Enter the number of productions.\n");	
+	scanf("%d",&n);
+	printf("Enter the productions in (LHS=RHS) format\n");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s",p[i]);
+	}
+	int _continue = 1;
+	do
+	{
+		char c;
+		printf("Enter the character to find the first and follow.\n");
+		scanf(" %c",&c);
+		first(c);
+		printf("First of %c is : {",c);
+		for(int i =0;i<m;i++)
+		{
+			printf("%c",f[i]);
+		}
+		printf("}\n");
+		strcpy(f," ");
+		m=0;
+		follow(c);
+		printf("Follow of %c is : {",c);
+		for(int i =0;i<m;i++)
+		{
+			printf("%c",f[i]);
+		}
+		printf("}\n");
+		printf("Continue? Enter 1\n");
+		scanf("%d",&_continue);
+	}
+	while(_continue ==1);
+	
 }
-
 void first(char c)
 {
-    if (!isupper(c))
-    {
-        f[k++] = c;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (p[i][0] == c)
-        {
-            if (p[i][2] == '$')
-            {
-                follow(p[i][0]);
-            }
-            else if (islower(p[i][2]))
-            {
-                f[k++] = p[i][2];
-            }
-            else
-            {
-                first(p[i][2]);
-            }
-        }
-    }
+	if(!isupper(c))
+	{
+		f[m] = c;
+		m++;
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(c==p[i][0])
+		{
+			if(p[i][2]=='$')
+			{
+				follow(p[i][2]);
+			}
+			else
+			{
+				first(p[i][2]);
+			}
+		}
+	}
+	
 }
 void follow(char c)
-{
-    if (p[0][0] == c)
-    {
-        f[k++] = '$';
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 2; j < strlen(p[i]); j++)
-        {
-            if (p[i][j] == c)
-            {
-                if (p[i][j + 1] != '\0')
-                {
-                    first(p[i][j + 1]);
-                }
-                else if (p[i][j + 1] == '\0' && c != p[i][0])
-                {
-                    follow(p[i][0]);
-                }
-            }
-        }
-    }
+{	
+	if(p[0][0] == c)
+	{
+		f[m] = '$';
+		m++;
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=2;j<strlen(p[i]);j++)
+		{
+			if(c==p[i][j])
+			{
+				if(p[i][j+1]!='\0')
+				{
+					first(p[i][j+1]);
+				}
+				else if(p[i][0]!=c)
+				{
+					follow(p[i][0]);
+				}
+			}
+		}
+	}	
 }
+
